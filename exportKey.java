b974efb6b0ecc3f4832c3bfb2b732a0cea720ad9
UranLajqi <58478405+UranLajqi@users.noreply.github.com>
2020-04-30 15:56:57 +0200

    Update exportKey.java

--- exportKey.java
+++ exportKey.java
@@ -1,29 +1,31 @@
 if(args[0].equals("export-key")) {
 	    	Boolean exist = FileExists(args[2], Path, ".xml");
-	    	if(exist) {
-		    	if(args[1].equals("public")) {
+			Boolean existsPu = FileExists(args[2], Path, ".pub.xml");
+	    	System.out.println(existsPu + "\n" + exist);
+			if(existsPu && args[1].equals("public")) {
 		    		System.out.println("<RSAKeyValue>");
 		    		System.out.println("    <Modulus>" + pubKey.getModulus() + "</Modulus>");
 		    		System.out.println("    <Exponent>" + pubKey.getPublicExponent() + "</Exponent>");
 		    		System.out.println("</RSAKeyValue>");
 		    	}
-		    	else if(args[1].equals("privat")) {
-		    		System.out.println("<RSAKeyValue>");
-		    		System.out.println("    <Modulus>" + privKey.getModulus() + "</Modulus>");
-		    		System.out.println("    <Exponent>" + privKey.getPublicExponent() + "</Exponent>");
-		    		System.out.println("    <P>" + privKey.getPrimeP() + "</P>");
-		    		System.out.println("    <Q>" + privKey.getPrimeQ() + "</Q>");
-		    		System.out.println("    <DP>" + privKey.getPrimeExponentP() + "</DP>");
-		    		System.out.println("    <DQ>" + privKey.getPrimeExponentQ() + "</DQ>");
-		    		System.out.println("    <InverseQ>" + privKey.getCrtCoefficient() + "</InverseQ>");
-		    		System.out.println("    <D>" + privKey.getPrivateExponent() + "</D>");
-		    		System.out.println("</RSAKeyValue>");
-		    	}
-		    	else {
-		    		System.out.println("gabim 1");
-		    	}
-	    	}
-	    
+			else if(!existsPu && args[1].equals("public")) {
+	    		System.out.println("Gabim: Celesi publik '" + args[1] + "' nuk ekziston.");
+			}
+			else if(exist && args[1].equals("privat")) {
+				System.out.println("<RSAKeyValue>");
+	    		System.out.println("    <Modulus>" + privKey.getModulus() + "</Modulus>");
+	    		System.out.println("    <Exponent>" + privKey.getPublicExponent() + "</Exponent>");
+	    		System.out.println("    <P>" + privKey.getPrimeP() + "</P>");
+	    		System.out.println("    <Q>" + privKey.getPrimeQ() + "</Q>");
+	    		System.out.println("    <DP>" + privKey.getPrimeExponentP() + "</DP>");
+	    		System.out.println("    <DQ>" + privKey.getPrimeExponentQ() + "</DQ>");
+	    		System.out.println("    <InverseQ>" + privKey.getCrtCoefficient() + "</InverseQ>");
+	    		System.out.println("    <D>" + privKey.getPrivateExponent() + "</D>");
+	    		System.out.println("</RSAKeyValue>");
+			}
+			else if(!exist && args[1].equals("privat")) {
+	    		System.out.println("Gabim: Celesi privat '" + args[1] + "' nuk ekziston.");
+			}
 	    	else {
 	    		System.out.println("Gabim 2");
 	    	}
