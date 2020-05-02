d6b932efaa92641b19d4d8b6bee105147313407c
LirimIslamii <61246827+LirimIslamii@users.noreply.github.com>
2020-05-03 00:01:34 +0200

    Update Metodat.java

--- Metodat.java
+++ Metodat.java
@@ -1,5 +1,3 @@
-
-
 import java.beans.XMLEncoder;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -17,11 +15,38 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 public class Metodat {
     private PrivateKey privateKey;
     private PublicKey publicKey;
-    
+    private static String Path = "C:\\\\Users\\\\Uran\\\\Desktop\\\\Projekti Siguri\\\\";
+
+    KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+    KeyPair keyPair = keyPairGen.genKeyPair();
+    RSAPrivateCrtKey privKey = (RSAPrivateCrtKey) keyPair.getPrivate();
+    RSAPublicKey pubKey = (RSAPublicKey) keyPair.getPublic();
+        
+    BigInteger n = privKey.getModulus();
+    BigInteger e = privKey.getPublicExponent();
+    BigInteger p = privKey.getPrimeP();
+    BigInteger q = privKey.getPrimeQ();
+    BigInteger dp = privKey.getPrimeExponentP();
+    BigInteger dq = privKey.getPrimeExponentQ();
+    BigInteger inverseQ = privKey.getCrtCoefficient();
+    BigInteger d = privKey.getPrivateExponent();
+        
+	String n1 = n.toString();
+	String e1 = e.toString();
+	String p1 = p.toString();
+	String q1 = q.toString();
+	String dp1 = dp.toString();
+	String dq1 = dq.toString();
+	String inverseQ1 = inverseQ.toString();
+	String d1 = d.toString();
+
     public Metodat() throws NoSuchAlgorithmException {
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         keyGen.initialize(1024);
@@ -38,15 +63,13 @@ public class Metodat {
         fos.flush();
         fos.close();
     }
+
     public static Boolean FileExists(String user, String path, String type){
         File tempFile = new File("" + path +user+type+"");
         boolean exists = tempFile.exists();
         return exists;
     }   
-    private static String Path = "C:\\\\Users\\\\Uran\\\\eclipse-workspace\\\\rsaexample\\\\";
-    public String argument;
-	Boolean exists = FileExists(argument, Path, ".xml");
-	Boolean existsPub = FileExists(argument, Path, ".pub.xml");
+
     public PrivateKey getPrivateKey() {
         return privateKey;
     }
@@ -61,41 +84,20 @@ public class Metodat {
 		try {
 		Cipher cipher = Cipher.getInstance("RSA");
 		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-		encryptedData = cipher.doFinal(dataToEncrypt);
-				
+		encryptedData = cipher.doFinal(dataToEncrypt);	
 		} catch ( NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
 			e.printStackTrace();
 		}
 		return encryptedData;
 	}
-    
-    KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
-    KeyPair keyPair = keyPairGen.genKeyPair();
-    RSAPrivateCrtKey privKey = (RSAPrivateCrtKey) keyPair.getPrivate();
-    RSAPublicKey pubKey = (RSAPublicKey) keyPair.getPublic();
-        
-        BigInteger n = privKey.getModulus();
-        BigInteger e = privKey.getPublicExponent();
-        BigInteger p = privKey.getPrimeP();
-        BigInteger q = privKey.getPrimeQ();
-        BigInteger dp = privKey.getPrimeExponentP();
-        BigInteger dq = privKey.getPrimeExponentQ();
-        BigInteger inverseQ = privKey.getCrtCoefficient();
-        BigInteger d = privKey.getPrivateExponent();
-        
-		String n1 = n.toString();
-		String e1 = e.toString();
-		String p1 = p.toString();
-		String q1 = q.toString();
-		String dp1 = dp.toString();
-		String dq1 = dq.toString();
-		String inverseQ1 = inverseQ.toString();
-		String d1 = d.toString();
-		
+
 		public void CreateUser(String argumenti2) throws IOException {
+			Boolean exists = FileExists(argumenti2, Path, ".xml");
+			Boolean existsPub = FileExists(argumenti2, Path, ".pub.xml");
+			if(!(exists && existsPub)){
 		    	Person w1 = new Person(n1,e1,p1,q1,dp1,dq1,inverseQ1,d1);
 		     
-		        FileOutputStream priv = new FileOutputStream(new File("./"+ argumenti2 +".xml"));
+		        FileOutputStream priv = new FileOutputStream(new File("C:\\\\Users\\\\Uran\\\\Desktop\\\\Projekti Siguri\\\\"+ argumenti2 +".xml"));
 				XMLEncoder encoder1 = new XMLEncoder(priv);
 				encoder1.writeObject(w1);
 				encoder1.close();
@@ -103,7 +105,7 @@ public class Metodat {
 				
 		        Person w2 = new Person(n1,e1);
 				
-				FileOutputStream pub = new FileOutputStream(new File("./"+ argumenti2 +".pub.xml"));
+				FileOutputStream pub = new FileOutputStream(new File("C:\\\\Users\\\\Uran\\\\Desktop\\\\Projekti Siguri\\\\"+ argumenti2 +".pub.xml"));
 				
 				XMLEncoder encoder2 = new XMLEncoder(pub);
 				encoder2.writeObject(w2);
@@ -113,25 +115,28 @@ public class Metodat {
 				System.out.println("Eshte krijuar celesi privat 'keys/" + argumenti2 + ".xml'");
 				System.out.println("Eshte krijuar celesi publik 'keys/" + argumenti2 + ".pub.xml'");
 			}
-
+			else{
+				System.out.print("Gabim: Celesi '" + argumenti2 + "' ekziston paraprakisht.");
+			}
+		}
 		public void Delete(String argumenti2) {
-				exists = FileExists(argumenti2, Path, ".xml");
-				existsPub = FileExists(argumenti2, Path, ".pub.xml");
+				Boolean exists = FileExists(argumenti2, Path, ".xml");
+				Boolean existsPub = FileExists(argumenti2, Path, ".pub.xml");
 		    	if(exists && existsPub) {
-		    		File file1 = new File("C:\\\\Users\\\\Uran\\\\eclipse-workspace\\\\rsaexample\\\\" + argumenti2 + ".pub.xml");
-		    		File file2 = new File("C:\\\\Users\\\\Uran\\\\eclipse-workspace\\\\rsaexample\\\\" + argumenti2 + ".xml");
+		    		File file1 = new File("./" + argumenti2 + ".pub.xml");
+		    		File file2 = new File("./" + argumenti2 + ".xml");
 		    		file1.delete();
 		    		file2.delete();
 		    		System.out.println("Eshte larguar celesi privat 'keys/" + argumenti2 + ".xml'");
 		    		System.out.println("Eshte larguar celesi publik 'keys/" + argumenti2 + ".pub.xml'");
 		    	}
 		    	else if(existsPub) {
-		    		File file1 = new File("C:\\\\Users\\\\Uran\\\\eclipse-workspace\\\\rsaexample\\\\" + argumenti2 + ".pub.xml");
+		    		File file1 = new File("./" + argumenti2 + ".pub.xml");
 		    		file1.delete();
 		    		System.out.println("Eshte larguar celesi publik 'keys/" + argumenti2 + ".pub.xml'");
 		    	}
 		    	else if(exists) {
-		    		File file2 = new File("C:\\\\Users\\\\Uran\\\\eclipse-workspace\\\\rsaexample\\\\" + argumenti2 + ".xml");
+		    		File file2 = new File("./" + argumenti2 + ".xml");
 		    		file2.delete();
 		    		System.out.println("Eshte larguar celesi privat 'keys/" + argumenti2 + ".xml'");
 		    	}
@@ -150,7 +155,7 @@ public class Metodat {
 		    		System.out.println("</RSAKeyValue>");
 		    	}
 				else if(!existsPu && argumenti2.equals("public")) {
-		    		System.out.println("Gabim: Celesi publik '" + argumenti2 + "' nuk ekziston.");
+		    		System.out.println("Gabim: Celesi publik '" + argumenti3 + "' nuk ekziston.");
 				}
 				else if(exist && argumenti2.equals("privat") && !argumenti2.endsWith(".xml")) {
 					System.out.println("<RSAKeyValue>");
@@ -165,16 +170,12 @@ public class Metodat {
 		    		System.out.println("</RSAKeyValue>");
 				}
 				else if(!exist && argumenti2.equals("privat")) {
-		    		System.out.println("Gabim: Celesi privat '" + argumenti2 + "' nuk ekziston.");
+		    		System.out.println("Gabim: Celesi privat '" + argumenti3 + "' nuk ekziston.");
 				}
 		    	else {
 		    		System.out.println("Gabim 2");
 		    	}
 		    }
-		// args[0] = argumenti1
-		// args[1] = argumenti2
-		// args[2] = argumenti3
-		// args[3] = argumenti4
 		public void Export2(String argumenti2, String argumenti3, String argumenti4) throws IOException {	
 			Boolean exist = FileExists(argumenti3, Path, ".xml");
 			Boolean existsPu = FileExists(argumenti3, Path, ".pub.xml");
@@ -214,25 +215,27 @@ public class Metodat {
 		
 		
 		public void Import(String argumenti1, String argumenti2) throws IOException {
-		    	Boolean existsPu = FileExists(argumenti2, "C:\\\\Users\\\\Uran\\\\eclipse-workspace\\\\rsaexample\\\\keys\\\\", "");
-				Boolean exist = FileExists(argumenti2, "C:\\\\Users\\\\Uran\\\\eclipse-workspace\\\\rsaexample\\\\keys\\\\", "");
+		    	Boolean existsPu = FileExists(argumenti2, "./", "");
+				Boolean exist = FileExists(argumenti2, "./", "");
 		    	if(!existsPu && argumenti2.endsWith(".pub.xml")) {
 			        Person w2 = new Person(n1,e1);
-					FileOutputStream pub = new FileOutputStream(new File("C:\\\\Users\\\\Uran\\\\eclipse-workspace\\\\rsaexample\\\\keys\\\\"+ argumenti2));
+					FileOutputStream pub = new FileOutputStream(new File("../"+ argumenti1));
 					XMLEncoder encoder2 = new XMLEncoder(pub);
 					encoder2.writeObject(w2);
 					encoder2.close();
 					pub.close();
-					System.out.println("Celesi publik u ruajt ne fajllin " + argumenti2);
+					System.out.println("Celesi publik u ruajt ne fajllin " + argumenti1 + ".pub.xml");
 		    	}
 		    	else if(!exist && argumenti2.endsWith(".xml")) {
-			    	Person w1 = new Person(n1,e1,p1,q1,dp1,dq1,inverseQ1,d1);
-			        FileOutputStream priv = new FileOutputStream(new File("C:\\\\Users\\\\Uran\\\\eclipse-workspace\\\\rsaexample\\\\keys\\\\"+argumenti2));
-					XMLEncoder encoder1 = new XMLEncoder(priv);
-					encoder1.writeObject(w1);
-					encoder1.close();
-					priv.close();
-					System.out.println("Celesi privat u ruajt ne fajllin " + argumenti2);
+			        Person w2 = new Person(n1,e1);
+					FileOutputStream pub = new FileOutputStream(new File("../"+ argumenti1));
+					XMLEncoder encoder2 = new XMLEncoder(pub);
+					encoder2.writeObject(w2);
+					encoder2.close();
+					pub.close();
+					System.out.println("Celesi publik u ruajt ne fajllin " + argumenti1);
+					
+					System.out.println("Eshte krijuar celesi privat 'keys/" + argumenti1 + ".xml'");
 		    	} 
 				else {
 					System.out.println("Gabim: Fajlli i dhene nuk eshte valid.");
@@ -271,7 +274,7 @@ public class Metodat {
 			String ciphertext = n11 + "." + n2 + "." + n3 + "." + n4;
 			
 			if(ar3.endsWith(".txt")) {
-	    		FileOutputStream priv = new FileOutputStream(new File("C:\\\\Users\\\\Uran\\\\eclipse-workspace\\\\rsaexample\\\\"+ ar3));
+	    		FileOutputStream priv = new FileOutputStream(new File("./"+ ar3));
 				XMLEncoder encoder1 = new XMLEncoder(priv);
 				encoder1.writeObject(ciphertext);
 				encoder1.close();
@@ -282,38 +285,44 @@ public class Metodat {
 				System.out.println(ciphertext);
     	}
 	    public void Write2(String ar1,String ar2) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {	
-	    	String n11 = Base64.getEncoder()
-                    .encodeToString(ar1.getBytes(StandardCharsets.UTF_8.toString()));
-    		
-	    	SecureRandom sr = new SecureRandom(); //create new secure random
-	    	byte [] iv = new byte[8]; //create an array of 8 bytes 
-	    	sr.nextBytes(iv); //create random bytes to be used for the IV (?) Not too sure.
-	    	String ivi = iv.toString();
-			String n2 = Base64.getEncoder()
-                    .encodeToString(ivi.getBytes(StandardCharsets.UTF_8.toString()));
-	    	
-	    	test rsaObj = new test();
-	    	SecureRandom sr1 = new SecureRandom(); //create new secure random
-    		byte [] k = new byte[8]; //create an array of 8 bytes 
-    		sr.nextBytes(k); //create random bytes to be used for the IV (?) Not too sure.    
-    		String ko = k.toString();
-			byte[] encryptedData = rsaObj.encryptData(ko);
-			String eno = encryptedData.toString();
-			String n3 = Base64.getEncoder()
-                    .encodeToString(eno.getBytes(StandardCharsets.UTF_8.toString()));
-			
-			SecretKey key = KeyGenerator.getInstance("DES").generateKey();
-			Cipher cipher = Cipher.getInstance("DES");
-			cipher.init(Cipher.ENCRYPT_MODE, key);
-			byte[] dataToEn = ar2.getBytes();
-			byte[] en = null;
-			String encrypted = cipher.doFinal(dataToEn).toString();
-			String n4 = Base64.getEncoder()
-                    .encodeToString(ar2.getBytes(StandardCharsets.UTF_8.toString()));
-	
-			String ciphertext = n11 + "." + n2 + "." + n3 + "." + n4;
-			
-				System.out.println(ciphertext);
+	    	Boolean existsPu = FileExists(ar1, "C:\\\\Users\\\\Uran\\\\Desktop\\\\Projekti Siguri\\\\", ".pub.xml");
+			if(existsPu){
+		    	String n11 = Base64.getEncoder()
+	                    .encodeToString(ar1.getBytes(StandardCharsets.UTF_8.toString()));
+	    		
+		    	SecureRandom sr = new SecureRandom(); //create new secure random
+		    	byte [] iv = new byte[8]; //create an array of 8 bytes 
+		    	sr.nextBytes(iv); //create random bytes to be used for the IV (?) Not too sure.
+		    	String ivi = iv.toString();
+				String n2 = Base64.getEncoder()
+	                    .encodeToString(ivi.getBytes(StandardCharsets.UTF_8.toString()));
+		    	
+		    	test rsaObj = new test();
+		    	SecureRandom sr1 = new SecureRandom(); //create new secure random
+	    		byte [] k = new byte[8]; //create an array of 8 bytes 
+	    		sr.nextBytes(k); //create random bytes to be used for the IV (?) Not too sure.    
+	    		String ko = k.toString();
+				byte[] encryptedData = rsaObj.encryptData(ko);
+				String eno = encryptedData.toString();
+				String n3 = Base64.getEncoder()
+	                    .encodeToString(eno.getBytes(StandardCharsets.UTF_8.toString()));
+				
+				SecretKey key = KeyGenerator.getInstance("DES").generateKey();
+				Cipher cipher = Cipher.getInstance("DES");
+				cipher.init(Cipher.ENCRYPT_MODE, key);
+				byte[] dataToEn = ar2.getBytes();
+				byte[] en = null;
+				String encrypted = cipher.doFinal(dataToEn).toString();
+				String n4 = Base64.getEncoder()
+	                    .encodeToString(ar2.getBytes(StandardCharsets.UTF_8.toString()));
+		
+				String ciphertext = n11 + "." + n2 + "." + n3 + "." + n4;
+				
+					System.out.println(ciphertext);
+			}
+			else{
+				System.out.println("Gabim: Celesi publik '" + ar1 + "' nuk ekziston.");
+			}
     	}
 	    
 	    
